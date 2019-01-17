66c98c715eb0b56fe085cca35609939668729708
Jonathan <joncwrrr@gmail.com>
2019-01-17 16:21:16 +0800

    lol

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     production: {
       user: 'ubuntu',
       host: 'ec2-18-191-129-57.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/tutoria.pem',
+      key: '~/.ssh/tutorial.pem',
       ref: 'origin/master',
       repo: 'git@github.com:Joncwr/aws-tutorial.git',
       path: '/home/ubuntu/aws-tutorial',
